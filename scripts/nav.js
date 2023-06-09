fe1c87e63fd5e68ab4e200b6db7ff816a21621db
Jakub Myrta <mkuba2006@gmail.com>
2023-06-09 12:22:26 +0200

    first commit

---
+++ scripts/nav.js
@@ -0,0 +1,40 @@
+const nav = document.querySelector('#nav1');
+
+const scroll = () =>{
+    const scrollPos = window.scrollY;
+    nav.classList.toggle("scroll-down", scrollPos > 56 );
+};
+document.addEventListener('scroll', scroll,{passive: true});
+
+
+
+const navl = document.querySelector("#burger1")
+const navi = document.querySelector("#secret")
+const nav_links = navi.querySelectorAll('a');
+const burger = document.querySelector("#burger1");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("toggle");
+  navi.classList.toggle("nav_active");
+  nav_links.forEach((link, index) => {
+    link.style.animation = `navLink 0.5s ease forwards ${index / 7}s`;
+  });
+});
+
+const hrefs = document.querySelectorAll("#secret a");
+const secretDiv = document.getElementById('secret');
+
+hrefs.forEach(href=>{
+  href.addEventListener('click', ()=>{
+    secretDiv.classList.remove('nav_active');
+    burger.classList.toggle("toggle");
+  })
+})
+
+const toggleMenu = () => document.body.classList.toggle('open');
+const as = document.querySelectorAll("body aside a");
+for(let i=0;i<as.length;i++){
+  as[i].addEventListener('click',()=>{
+    document.body.classList.remove('open');
+    burger.classList.toggle("toggle");
+  })
+}
