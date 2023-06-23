e11179141453bbf44598a2ab3b82115af6ce1112
Jakub Myrta <mkuba2006@gmail.com>
2023-06-23 23:48:01 +0200

    23.06

---
+++ scripts/news-script.js
@@ -0,0 +1,18 @@
+import news from './news.js';
+const newses = news;
+
+const container = document.querySelector('#news');
+for(let i of newses){
+    const div = document.createElement('div');
+    div.className ='container';
+    div.innerHTML =`
+        <img src='${i.img}'/>
+        <div>    
+            <time datetime="2023-06-24" class="card__date">${i.data}</time>
+            <h1>${i.title}</h1>
+            <p>${i.discription}</p>
+        </div>
+
+    `;
+    container.appendChild(div);
+}
---
+++ scripts/news.js
@@ -0,0 +1,27 @@
+const news =[
+    {
+        title:'Zakończene Roku 2022/2023',
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLP_SHzwbLHlVBB2Mx0fCCg03Rvnspk_iiQw&usqp=CAU",
+        discription:'NONE',
+        data:'24 czerwca 2023',
+    },
+    {
+        title:'Zakończene Roku 2022/2023',
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjOOrJmihL6kZmOLb8VAOrzR_1erLYtwlxRg&usqp=CAU",
+        discription:'NONE',
+        data:'1 września 2022',
+    },
+    {
+        title:'Boisko',
+        img:"https://foto.cloudgdansk.pl/foto/Otwarcie_boiska_przy_IX_LO_9763_800px.JPG",
+        discription:'NONE',
+        data:'24 czerwca 2023',
+    },
+    {
+        title:'Turniej',
+        img:"https://9liceum.pl/wp-content/uploads/2021/05/plakat-1.jpg",
+        discription:'NONE',
+        data:'24 czerwca 2023',
+    },
+]
+export default news
