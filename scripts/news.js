2c1aa7421b6e88eb2316b4bf7b00bb4cff7360fb
Jakub Myrta <mkuba2006@gmail.com>
2023-07-27 23:00:13 +0200

    1

--- scripts/news.js
+++ scripts/news.js
@@ -2,26 +2,26 @@ const news =[
     {
         title:'Zakończene Roku 2022/2023',
         img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLP_SHzwbLHlVBB2Mx0fCCg03Rvnspk_iiQw&usqp=CAU",
-        discription:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, in. Accusantium, alialibero laudantium possimus vero dolorum?',
+        discription:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, in.',
         data:'24 czerwca 2023',
     },
     {
-        title:'Zakończene Roku 2022/2023',
+        title:'Super Wfista',
         img:"https://zaruski.pl/wp-content/uploads/2021/10/FHD_DSC6263.jpg",
-        discription:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, in. Accusantium, alialibero laudantium possimus vero dolorum?',
+        discription:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, in. Accusantium, alialibero?',
         data:'1 września 2022',
     },
     {
-        title:'Boisko',
-        img:"https://foto.cloudgdansk.pl/foto/Otwarcie_boiska_przy_IX_LO_9763_800px.JPG",
-        discription:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, in. Accusantium, alialibero laudantium possimus vero dolorum?',
+        title:'Nowy Dyrektor',
+        img:"https://media.licdn.com/dms/image/C4E22AQF7KbPUIHLF3g/feedshare-shrink_800/0/1656764799386?e=1693440000&v=beta&t=Npe0P7bcXdm36Fl8dz418lCYiGcH8IXQJ2t6MYzBBjE",
+        discription:'Wraz z dniem 1 wrześnai 2024 roku, nowym dyrektorem zostanie Zygmunt Pilarek',
         data:'24 czerwca 2023',
     },
     {
         title:'Turniej',
         img:"https://9liceum.pl/wp-content/uploads/2021/05/plakat-1.jpg",
-        discription:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, in. Accusantium, alialibero laudantium possimus vero dolorum?',
-        data:'24 czerwca 2023',
+        discription:'Uprzejmie informujemy iż w dniach 15-19 maja odbywać się będą turnieje szkolne z piłki nożnej oraz Siatków. Zespoły 6 osobowe.',
+        data:'9 maja 2023',
     },
 ]
 export default news
