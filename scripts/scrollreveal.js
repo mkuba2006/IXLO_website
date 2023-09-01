9b3b1ef9a87942600410f5fe2cf7ce95aed7c87a
Jakub Myrta <mkuba2006@gmail.com>
2023-09-02 01:15:18 +0200

    12

--- scripts/scrollreveal.js
+++ scripts/scrollreveal.js
@@ -1,6 +1,16 @@
 ScrollReveal().reveal('#one img', {origin: 'left', distance: '10%'});
 
-ScrollReveal().reveal('#but a', {origin: 'left', distance: '10%', delay:'200'});
+
+const bur = document.getElementById('burger1');
+bur.addEventListener('click',()=>{
+    console.log('click');
+    ScrollReveal().reveal('#but a', {origin: 'left', distance: '10%', delay:'200'});
+})
+
+
+
+
+
 ScrollReveal().reveal('#wiadomosci #title', {origin: 'left', distance: '10%'});
 
 ScrollReveal().reveal('#tiles .tile', {origin: 'bottom', distance: '40%', interval: 100,  delay:'300'});
