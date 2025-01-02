f8e71c20fd4bd537c11ee444694a659bc76bf455
Jakub Myrta <mkuba2006@gmail.com>
2025-01-02 21:22:47 +0100

    x

--- scripts/scrollreveal.js
+++ scripts/scrollreveal.js
@@ -1,10 +1,5 @@
 ScrollReveal().reveal('#one img', {origin: 'left', distance: '10%'});
 
-
-
-
-
-
 ScrollReveal().reveal('#wiadomosci #title', {origin: 'left', distance: '10%'});
 
 ScrollReveal().reveal('#tiles .tile', {origin: 'bottom', distance: '40%', interval: 100,  delay:'300'});
